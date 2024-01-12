3bdd84f29f7f64c6dbca8121747dd1a8a95930b8
Simpkins717 <dsimpkins717@gmail.com>
2024-01-12 15:17:56 -0500

    added input validation on creating a topic

---
+++ redditclonenoformatting/src/actions/create-topic.ts
@@ -0,0 +1,37 @@
+'use server';
+import { z } from 'zod';
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: 'Must be lowercase letters or dashes without spaces',
+    }),
+  description: z.string().min(7),
+});
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+  };
+}
+
+export async function createTopic(
+  formState: CreateTopicFormState,
+  formData: FormData
+): Promise<CreateTopicFormState> {
+  //TODO: revalidate the homepage
+  const result = createTopicSchema.safeParse({
+    name: formData.get('name'),
+    description: formData.get('description'),
+  });
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+  return {
+    errors: {},
+  };
+}
--- redditclonenoformatting/src/app/page.tsx
+++ redditclonenoformatting/src/app/page.tsx
@@ -1,26 +1,13 @@
-import { Button } from '@nextui-org/react';
-import * as actions from '@/actions';
-import { auth } from '@/auth';
-import Profile from '@/components/profile';
-
-export default async function Home() {
-  const session = await auth();
-  if (session?.user) {
-    return (
-      <div>
-        <form action={actions.signOut}>
-          <Button type='submit'>Sign Out</Button>
-        </form>
+import TopicCreateForm from '@/components/topics/topic-create-form';
+export default function Home() {
+  return (
+    <div className='grid grid-cols-4 gap-4 p-4 '>
+      <div className='col-span-3'>
+        <h1 className='text-xl m-2'>Top Posts</h1>
       </div>
-    );
-  } else {
-    return (
       <div>
-        <Profile />
-        <form action={actions.signIn}>
-          <Button type='submit'>Sign In</Button>
-        </form>
+        <TopicCreateForm />
       </div>
-    );
-  }
+    </div>
+  );
 }
