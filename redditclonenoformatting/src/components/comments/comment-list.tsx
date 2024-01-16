d8a893f8288237d93e07133ca3c53ef388ac9eee
Simpkins717 <dsimpkins717@gmail.com>
2024-01-16 17:42:09 -0500

    using cache to prevent multiple requests to the server for the same data

--- redditclonenoformatting/src/components/comments/comment-list.tsx
+++ redditclonenoformatting/src/components/comments/comment-list.tsx
@@ -1,25 +1,29 @@
-import CommentShow from "@/components/comments/comment-show";
+import CommentShow from '@/components/comments/comment-show';
+import {
+  CommentWithAuthor,
+  fetchCommentsByPostId,
+} from '@/db/queries/comments';
 
-interface CommentListProps {}
+interface CommentListProps {
+  postId: string;
+}
 
 // TODO: Get a list of comments from somewhere
-export default function CommentList({}: CommentListProps) {
+export default async function CommentList({ postId }: CommentListProps) {
+  const comments = await fetchCommentsByPostId(postId);
+
   const topLevelComments = comments.filter(
     (comment) => comment.parentId === null
   );
   const renderedComments = topLevelComments.map((comment) => {
     return (
-      <CommentShow
-        key={comment.id}
-        commentId={comment.id}
-        comments={comments}
-      />
+      <CommentShow key={comment.id} commentId={comment.id} postId={postId} />
     );
   });
 
   return (
-    <div className="space-y-3">
-      <h1 className="text-lg font-bold">All {comments.length} comments</h1>
+    <div className='space-y-3'>
+      <h1 className='text-lg font-bold'>All {comments.length} comments</h1>
       {renderedComments}
     </div>
   );
