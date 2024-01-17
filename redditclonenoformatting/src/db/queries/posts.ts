c6308191a5dd7674d17cad06c0489d54dcefb3d9
Simpkins717 <dsimpkins717@gmail.com>
2024-01-17 10:37:54 -0500

    added suspense on search input

--- redditclonenoformatting/src/db/queries/posts.ts
+++ redditclonenoformatting/src/db/queries/posts.ts
@@ -18,6 +18,19 @@ export function fetchPostsByTopicSlug(slug: string): Promise<PostWithData[]> {
   });
 }
 
+export function fetchPostsBySearchTerm(term: string): Promise<PostWithData[]> {
+  return db.post.findMany({
+    include: {
+      topic: { select: { slug: true } },
+      user: { select: { name: true, image: true } },
+      _count: { select: { comments: true } },
+    },
+    where: {
+      OR: [{ title: { contains: term } }, { content: { contains: term } }],
+    },
+  });
+}
+
 export function fetchTopPosts(): Promise<PostWithData[]> {
   return db.post.findMany({
     orderBy: [
