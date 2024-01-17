3164c1fc53aa8abd2d1c17da9647dfa125f59a96
Simpkins717 <dsimpkins717@gmail.com>
2024-01-17 08:51:03 -0500

    scaffolding homepage fetch posts query

--- redditclonenoformatting/src/app/topics/[slug]/posts/[postId]/page.tsx
+++ redditclonenoformatting/src/app/topics/[slug]/posts/[postId]/page.tsx
@@ -2,8 +2,11 @@ import Link from 'next/link';
 import PostShow from '@/components/posts/post-show';
 import CommentList from '@/components/comments/comment-list';
 import CommentCreateForm from '@/components/comments/comment-create-form';
+import { Suspense } from 'react';
 import paths from '@/paths';
 import { fetchCommentsByPostId } from '@/db/queries/comments';
+import { divider } from '@nextui-org/react';
+import PostShowLoading from '@/components/posts/post-show-loading';
 
 interface PostShowPageProps {
   params: {
@@ -20,7 +23,9 @@ export default async function PostShowPage({ params }: PostShowPageProps) {
       <Link className='underline decoration-solid' href={paths.topicShow(slug)}>
         {'< '}Back to {slug}
       </Link>
-      <PostShow postId={postId} />
+      <Suspense fallback={<PostShowLoading />}>
+        <PostShow postId={postId} />
+      </Suspense>
       <CommentCreateForm postId={postId} startOpen />
       <CommentList postId={postId} />
     </div>
